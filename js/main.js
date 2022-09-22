d36801c38c7213d19f1284fe56be48304b53a162
= <=>
2022-09-22 17:23:10 +0900

    오류 수정

--- js/main.js
+++ js/main.js
@@ -1,87 +1,79 @@
 $(document).ready(function () {
-
-    $(function() {  
-
-        // body
-        $("body").niceScroll({
-            cursorwidth: "10px",
-            railpadding: { top: 0, right: 10, left: 0, bottom: 0 }
-        });
-        // po1
-        // $(".po1").niceScroll({
-        //     cursorcolor: "#29306A",
-        //     cursoropacitymax: 0.8,
-        //     cursorwidth: "5px",
-        //     cursorborder: "1px solid transparent",
-        //     railpadding: { top: 0, right: 2, left: 0, bottom: 0 }
-        // });
-
+  $(function () {
+    // body
+    $("body").niceScroll({
+      cursorwidth: "10px",
+      railpadding: { top: 0, right: 10, left: 0, bottom: 0 },
     });
-
+    // po1
+    // $(".po1").niceScroll({
+    //     cursorcolor: "#29306A",
+    //     cursoropacitymax: 0.8,
+    //     cursorwidth: "5px",
+    //     cursorborder: "1px solid transparent",
+    //     railpadding: { top: 0, right: 2, left: 0, bottom: 0 }
+    // });
+  });
 });
 
 window.onload = function () {
+  // AOS 실행
+  AOS.init();
 
-    // AOS 실행
-    AOS.init();
+  // waypoint 옵션
 
-    // waypoint 옵션
-    
-    function MyWaypoint(_what, _effect, _offset, _delay) {
-
-        _what.each(function (index, el) {
-
-            new Waypoint({
-                element: el,
-                handler: function (direction) {
-                    var element = $(this.element);
-                    setTimeout(function () {
-                        element.addClass(_effect);
-                    }, _delay);
-                    // this.destroy();
-                },
-                offset: _offset
-            });
-
-        });
-    }
-
-    // on/off
-    function MyWaypointOnOff(_fromWhere, _what, _onClass, _offset) {
-
-        new Waypoint({
-            element: _fromWhere,
-            handler: function (direction) {
-                if (direction == 'down') {
-                    _what.addClass(_onClass);
-                } else if (direction == 'up') {
-                    _what.removeClass(_onClass);
-                }
-            },
-            offset: _offset
-        });
-
-    }
-    
-    MyWaypointOnOff($('.About_Me'), $('.sideMenu_wrap'), 'sideMenu_wrap_on', '50%'); // sideMenu on/off
-
-    //  ani 
-
-    // MyWaypoint($('.pf_bg_1'), 'down-go', '10%', 0); // pf_bg
-    MyWaypoint($('.portfolio-part_2 .headerBox'), 'togolong-left', '70%', 0); // headerBox
-    MyWaypoint($('.portfolio-part_3 .headerBox'), 'togolong-right', '70%', 0); // headerBox
-
-
-    // swiper
-
-    // study swiper img
-    let swStudy_Img = new Swiper('.sw_study_img', {
-        loop: true,
-        autoplay: true,
-        slidesPerView: 4,
-        spaceBetween: 30,
-        speed: 1400
+  function MyWaypoint(_what, _effect, _offset, _delay) {
+    _what.each(function (index, el) {
+      new Waypoint({
+        element: el,
+        handler: function (direction) {
+          var element = $(this.element);
+          setTimeout(function () {
+            element.addClass(_effect);
+          }, _delay);
+          // this.destroy();
+        },
+        offset: _offset,
+      });
     });
+  }
 
+  // on/off
+  function MyWaypointOnOff(_fromWhere, _what, _onClass, _offset) {
+    new Waypoint({
+      element: _fromWhere,
+      handler: function (direction) {
+        if (direction == "down") {
+          _what.addClass(_onClass);
+        } else if (direction == "up") {
+          _what.removeClass(_onClass);
+        }
+      },
+      offset: _offset,
+    });
+  }
 
-}
+  MyWaypointOnOff(
+    $(".About_Me"),
+    $(".sideMenu_wrap"),
+    "sideMenu_wrap_on",
+    "50%"
+  ); // sideMenu on/off
+
+  //  ani
+
+  MyWaypoint($(".pf_bg_1"), "down-go", "10%", 0); // pf_bg
+  MyWaypoint($(".portfolio-part_2 .headerBox"), "togolong-left", "70%", 0); // headerBox
+  MyWaypoint($(".portfolio-part_3 .headerBox"), "togolong-right", "70%", 0); // headerBox
+
+  // swiper
+
+  // study swiper img
+  let swStudy_Img = new Swiper(".sw_study_img", {
+    loop: true,
+    autoplay: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    speed: 1400,
+  });
+};
